a624d67114f5b6728f896c1254b5a1941b8d6fa7
James Birchfield <jbirchfield@popsugar.com>
2017-11-18 17:09:21 -0500

    initial import

---
+++ src/main/java/com/shopstyle/dl/fashion/FashionApplication.java
@@ -0,0 +1,12 @@
+package com.shopstyle.dl.fashion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FashionApplication.class, args);
+	}
+}
---
+++ src/test/java/com/shopstyle/dl/fashion/FashionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.shopstyle.dl.fashion;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FashionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
