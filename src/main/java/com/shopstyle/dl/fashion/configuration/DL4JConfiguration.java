b45cffc613ceb4c2a135d36c9677f07a5eb7d92f
James Birchfield <jbirchfield@popsugar.com>
2017-11-18 20:25:10 -0500

    stuff

--- src/main/java/com/shopstyle/dl/fashion/configuration/DL4JConfiguration.java
+++ src/main/java/com/shopstyle/dl/fashion/configuration/DL4JConfiguration.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Random;
 
+import org.apache.log4j.Logger;
 import org.datavec.api.io.labels.ParentPathLabelGenerator;
 import org.datavec.api.records.reader.RecordReader;
 import org.datavec.api.split.FileSplit;
@@ -14,6 +15,7 @@ import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
 import org.deeplearning4j.nn.api.Model;
 import org.deeplearning4j.nn.api.OptimizationAlgorithm;
 import org.deeplearning4j.nn.conf.Updater;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
 import org.deeplearning4j.nn.conf.layers.OutputLayer;
 import org.deeplearning4j.nn.graph.ComputationGraph;
 import org.deeplearning4j.nn.transferlearning.FineTuneConfiguration;
@@ -35,6 +37,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class DL4JConfiguration {
 
+	private static final Logger log = org.slf4j.LoggerFactory.getLogger(DL4JConfiguration.class);
 	private static final String[] allowedExtensions = BaseImageLoader.ALLOWED_FORMATS;
 
 	private static final long seed = 12345;
@@ -44,6 +47,7 @@ public class DL4JConfiguration {
 	private static final int height = 224;
 	private static final int width = 224;
 	private static final int channels = 3;
+	private static final String featureExtractionLayer = "block5_pool";
 
 	@Bean
 	public Model vgg16() {
@@ -54,9 +58,10 @@ public class DL4JConfiguration {
 		double rate = 0.0015; // learning rate
 		int numClasses = 2;
 
-		FineTuneConfiguration fineTuneConf = new FineTuneConfiguration.Builder().learningRate(rate)
+		FineTuneConfiguration fineTuneConf = new FineTuneConfiguration.Builder().activation(Activation.LEAKYRELU)
+				.weightInit(WeightInit.RELU).learningRate(5e-5)
 				.optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT).updater(Updater.NESTEROVS)
-				.seed(rngSeed).build();
+				.dropOut(0.5).seed(seed).build();
 
 		ZooModel zooModel = new VGG16();
 		ComputationGraph pretrainedNet = null;
@@ -68,22 +73,39 @@ public class DL4JConfiguration {
 		pretrainedNet.init();
 
 		ComputationGraph vgg16Transfer = new TransferLearning.GraphBuilder(pretrainedNet)
-				.fineTuneConfiguration(fineTuneConf).setFeatureExtractor("fc2")
-				.removeVertexKeepConnections("predictions")
-				.addLayer("predictions",
-						new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD).nIn(4096)
-								.nOut(numClasses).weightInit(WeightInit.XAVIER).activation(Activation.SOFTMAX).build(),
-						"fc2")
+				.fineTuneConfiguration(fineTuneConf).setFeatureExtractor(featureExtractionLayer) // "block5_pool" and
+																									// below are frozen
+				.nOutReplace("fc2", 1024, WeightInit.XAVIER) // modify nOut of the "fc2" vertex
+				.removeVertexAndConnections("predictions") // remove the final vertex and it's connections
+				.addLayer("fc3", new DenseLayer.Builder().activation(Activation.TANH).nIn(1024).nOut(256).build(),
+						"fc2") // add in a new dense layer
+				.addLayer("newpredictions",
+						new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+								.activation(Activation.SOFTMAX).nIn(256).nOut(numClasses).build(),
+						"fc3") // add in a final output dense layer,
+								// note that learning related configurations applied on a new layer here will be
+								// honored
+								// In other words - these will override the finetune confs.
+								// For eg. activation function will be softmax not RELU
+				.setOutputs("newpredictions") // since we removed the output vertex and it's connections we need to
+												// specify outputs for the graph
 				.build();
-
+		System.out.println(vgg16Transfer.summary());
 
 		RecordReaderDataSetIterator rrdi = new RecordReaderDataSetIterator(loadData(), 4, 1, numClasses);
-        vgg16Transfer.setListeners(new ScoreIterationListener());
+		vgg16Transfer.setListeners(new ScoreIterationListener());
 
-        TransferLearningHelper transferLearningHelper = new TransferLearningHelper(vgg16Transfer);
+		TransferLearningHelper transferLearningHelper = new TransferLearningHelper(vgg16Transfer);
+
+		// transferLearningHelper.fitFeaturized(rrdi);
 
 		System.out.println("Fitting....");
-        transferLearningHelper.fitFeaturized(rrdi);
+		int iter = 0;
+		while (rrdi.hasNext()) {
+			vgg16Transfer.fit(rrdi.next());
+			log.info("iter " + iter + " ....");
+			iter++;
+		}
 		System.out.println("Fit.");
 
 		File dir = new File(System.getProperty("user.home"), "/data/dogscats/train/cats");
